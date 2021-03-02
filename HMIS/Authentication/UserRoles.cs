60f32106f546b2c141a331618bc523aa8c401872
Arslan Zafar <arslanhisdu250@gmail.com>
2021-03-02 16:34:36 +0500

    Second Commit

---
+++ HMIS/Authentication/UserRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace HMIS.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public string UserId { get; set; }
+        public string[] AddRoleId { get; set; }
+        public string[] DeleteRoleId { get; set; }
+    }
+}
--- HMIS/Startup.cs
+++ HMIS/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HMIS.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,11 +18,11 @@ namespace HMIS
     {
         public Startup(IConfiguration configuration)
         {
-            // Configuration = configuration;
-            Configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                //.AddJsonFile("bundleconfig.json")
-                .Build();
+            Configuration = configuration;
+            //Configuration = new ConfigurationBuilder()
+            //    .AddJsonFile("appsettings.json")
+            //    //.AddJsonFile("bundleconfig.json")
+            //    .Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -27,7 +30,17 @@ namespace HMIS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddControllersWithViews();
+
+            // For Entity Framework
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnStr")));
+
+            // For Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,17 +58,20 @@ namespace HMIS
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
         }
     }
 }
